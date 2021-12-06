abc0636e51cc448bd7694e28b7d3960ccb86fd23
Marc Baetica <marc.baetica@gmail.com>
2021-12-05 19:36:27 -0500

    Draw direction refactor.

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from time import sleep
 PEN_WIDTH = 5
 
 
-
 """
+PATTERN
 8 up
 8 right
 6 down
@@ -30,7 +30,21 @@ def set_window_size(width, height):
     pass
 
 
-def draw_borders():
+def draw_in_direction(units_count, direction='left', angle=0):
+    if direction == 'left':
+        left(angle)
+    elif direction == 'right':
+        right(angle)
+    else:
+        raise ValueError(f'Direction can be either left or right. Received value: {direction}')
+    forward(units_count * PEN_WIDTH)
+
+
+def draw_horizontal_line_at_position(y):
+    pass
+
+
+def draw_borders(units_between_borders):
     penup()
     backward(window_width()/2)
     pendown()
@@ -38,7 +52,7 @@ def draw_borders():
     penup()
     backward(window_width())
     left(90)
-    forward(12*PEN_WIDTH)
+    forward(units_between_borders*PEN_WIDTH)
     right(90)
     pendown()
     forward(window_width())
@@ -52,34 +66,23 @@ def draw_pattern():
     left(90)
     pendown()
     for _ in range(window_width()//(PEN_WIDTH*10)+1):  # TODO: round up
-        forward(1*PEN_WIDTH)
-        left(90)
-        forward(8*PEN_WIDTH)
-        right(90)
-        forward(8*PEN_WIDTH)
-        right(90)
-        forward(6*PEN_WIDTH)
-        right(90)
-        forward(4*PEN_WIDTH)
-        right(90)
-        forward(2*PEN_WIDTH)
-        right(90)
-        forward(2*PEN_WIDTH)
-        left(90)
-        forward(2*PEN_WIDTH)
-        left(90)
-        forward(4*PEN_WIDTH)
-        left(90)
-        forward(6*PEN_WIDTH)
-        left(90)
-        forward(7*PEN_WIDTH)
+        draw_in_direction(1)
+        draw_in_direction(8, 'left', 90)
+        draw_in_direction(8, 'right', 90)
+        draw_in_direction(6, 'right', 90)
+        draw_in_direction(4, 'right', 90)
+        draw_in_direction(2, 'right', 90)
+        draw_in_direction(2, 'right', 90)
+        draw_in_direction(2, 'left', 90)
+        draw_in_direction(4, 'left', 90)
+        draw_in_direction(6, 'left', 90)
+        draw_in_direction(7, 'left', 90)
 
 
-set_pen_characteristics(PEN_WIDTH, 100)
+set_pen_characteristics(PEN_WIDTH, 20)
 print(window_width(), window_height())
 # set_window_size(width, height)
 
-sleep(3)
-draw_borders()
+draw_borders(12)
 draw_pattern()
-sleep(5)
+# sleep(5)
